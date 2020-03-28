ec4dedf160267978dbdf68ca2a4e83ae4fd83f5d
Zach White <zechnwhite@gmail.com>
2020-03-27 22:14:17 -0700

    promise.all in getdata

--- user/getdata.js
+++ user/getdata.js
@@ -15,56 +15,62 @@ exports.handler = async event => {
     text: 'SELECT id, firstname, lastname, email FROM users WHERE id = $1',
     values: [userid]
   };
-  
+
   const getPlayers = {
-    text: 'SELECT player_id, firstname, lastname, team_id, jersey FROM players WHERE user_id = $1',
+    text:
+      'SELECT player_id, firstname, lastname, team_id, jersey FROM players WHERE user_id = $1',
     values: [userid]
   };
-  
+
   try {
     const teamData = await pool.query(getTeams);
     const userData = await pool.query(getUserData);
     const playerData = await pool.query(getPlayers);
-    
-    let teams = [];
-    for (let i = 0; i < teamData.rows.length; i++) {
-      let team = {
-        id: teamData.rows[i].team_id,
-        name: teamData.rows[i].team_name
-      }
-      teams.push(team);
-      continue;
-    }
-    
-    let players = [];
-    for(let i =0; i < playerData.rows.length; i++){
-      let player = {
-        id: playerData.rows[i].player_id,
-        firstname: playerData.rows[i].firstname,
-        lastname: playerData.rows[i].lastname,
-        team_id: playerData.rows[i].team_id,
-        jersey: playerData.rows[i].jersey
-      }
-      players.push(player);
-    }
 
-    const response = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        data: {
-          id: userData.rows[0].user_id,
-          firstname: userData.rows[0].firstname,
-          lastname: userData.rows[0].lastname,
-          email: userData.rows[0].email,
-          teams: teams,
-          players: players
-        }
-      })
-    };
-    return response;
+    Promise.all([teamData, userData, playerData]).then(values => {
+      const returnedTeams = values[0];
+      const returnedUserData = values[1];
+      const returnedPlayers = values[2];
+
+      let teams = [];
+      for (let i = 0; i < returnedTeams.rows.length; i++) {
+        let team = {
+          id: returnedTeams.rows[i].team_id,
+          name: returnedTeams.rows[i].team_name
+        };
+        teams.push(team);
+        continue;
+      }
+      
+      let players = [];
+      for (let i = 0; i < returnedPlayers.rows.length; i++) {
+        let player = {
+          id: returnedPlayers.rows[i].player_id,
+          firstname: returnedPlayers.rows[i].firstname,
+          lastname: returnedPlayers.rows[i].lastname,
+          team_id: returnedPlayers.rows[i].team_id,
+          jersey: returnedPlayers.rows[i].jersey
+        };
+        players.push(player);
+      }
+      const response = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          data: {
+            id: returnedUserData.rows[0].user_id,
+            firstname: returnedUserData.rows[0].firstname,
+            lastname: returnedUserData.rows[0].lastname,
+            email: returnedUserData.rows[0].email,
+            teams: teams,
+            players: players
+          }
+        })
+      };
+      return response;
+    })
   } catch (err) {
     const response = {
       headers: {
