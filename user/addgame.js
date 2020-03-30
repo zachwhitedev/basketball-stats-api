031652dea34817b4bd37919f27b7c8d54256594d
Zach White <zechnwhite@gmail.com>
2020-03-29 19:30:20 -0700

    all adding data routes functional

---
+++ user/addgame.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const pool = require('../db');
+
+exports.handler = async event => {
+
+  const bodyData = event.body;
+  const game = bodyData.game;
+
+  const gamename = game.gamename;
+  const userid = game.userid;
+  const teamid = game.teamid;
+
+  const addGame = {
+    text:
+      'INSERT INTO games (game_name, user_id, team_id) VALUES ($1, $2, $3)',
+    values: [gamename, userid, teamid]
+  };
+
+  try {
+    const addedGame = await pool.query(addGame);
+    if(addedGame.rowCount > 0){
+        const response = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: {
+            message: `New game '${gamename}' added successfully`
+          }
+        };
+        return response;
+    } else console.log('Problem writing to database.')
+  } catch (err) {
+    const response = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: err
+      })
+    };
+    console.log(err);
+    return response;
+  }
+};
